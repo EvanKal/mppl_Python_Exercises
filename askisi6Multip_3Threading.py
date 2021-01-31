0a48cff15edac32b5233f0475f3258e991c80a06
Evangelos Kal <kalabokisevangelos@gmail.com>
2021-01-31 20:56:49 +0200

    Completed Askisi 6 with multi threading

--- askisi6Multip_3Threading.py
+++ askisi6Multip_3Threading.py
@@ -20,6 +20,7 @@ gameID = "1100"
 firstID = ""
 winningNumbersOfFirstDraw = {}
 allDrawsResults = []
+threadIndent=0
 
 def initiate():
     
@@ -27,16 +28,8 @@ def initiate():
     displayStatisticsPerDay(allDrawsResults)
 
 def getAllDrawResultsForCurrentMonth():
-    #API returns all draw results for one day at a time
     allDaysToRetrieve = []
-    allDrawsResults = []
-    dayToRetrieveDraws = datetime.date.today().replace(day=1)
-    
-    while today > dayToRetrieveDraws:
-        allDaysToRetrieve.append(dayToRetrieveDraws)
-        nextDay = dayToRetrieveDraws.day + 1
-        dayToRetrieveDraws = dayToRetrieveDraws.replace(day=nextDay)
-    
+    allDaysToRetrieve  = [date for date in calendar.Calendar().itermonthdates(today.year, today.month)]  
     
     multiProcessCallsToAPI(allDaysToRetrieve)
 
@@ -45,49 +38,67 @@ def multiProcessCallsToAPI(allDaysToRetrieve):
     if __name__ == '__main__':        
         lock = Lock()
 
-        #Gather all processes in a list so that I can check whether all of them have terminated
-        allThreads = [Thread(target=getResultsPerDay, args=(lock, day)) for day in allDaysToRetrieve]
+        # Create an event to handle the way the results from the call get gathered
+        e = threading.Event()
+
+        #Gather all processes in a list so that I can handle their execution
+        allThreads = [Thread(target=getResultsPerDay, args=(lock, e, day, index)) for index, day in enumerate(allDaysToRetrieve)]
           
-        #Fire all API calls concurrently in multpleThreads
+        #Fire all API calls concurrently in multple Threads with no concern about index
         for thread in allThreads:
             thread.start()
         
-        #Join all threads after they have been fired concurrrently
+        #Join all threads after they have been fired concurrrently so that they can terminate gracefully
         for thread in allThreads:
             thread.join()
 
-def getResultsPerDay(l, dayToRetrieveDraws):
+def getResultsPerDay(l, e, dayToRetrieveDraws, index):
     global allDrawsResults
+    global threadIndent
+    
+    #API returns all draw results for one day at a time so limit is set to 180 to get all draw results in one call, but seperate calls for each day need to be made
     url = "https://api.opap.gr/draws/v3.0/{gameId}/draw-date/{fromDate}/{toDate}?limit=180&property=winningNumbers".format(gameId = gameID, fromDate =  dayToRetrieveDraws, toDate = dayToRetrieveDraws)
     print(url)
     results = getDictFromJSONResponse(url)
     results['date']  = dayToRetrieveDraws
+    
+    #Wait until previous threads have completed writing to global variable 'allDrawsResults'
+    while index != threadIndent:
+        e.wait()
 
+    #If thread index is the one that should write to 'allDrawsResults' then go on
     l.acquire()
     try:
+        print("Released thread with index ", index)
         allDrawsResults.append(results)
+        threadIndent = index + 1
     finally:
-        l.release() 
+        l.release()
+        #Set event to true for next thread to release and write the results it holds to 'allDrawsResults'
+        e.set()
 
 def displayStatisticsPerDay(allDrawsResults):
     print("Showing results for current month: {monthDescr}".format(monthDescr=monthDescr))
 
-    #Itterate over every result per day that contains all 180 draw results of the day
-    for resultSet in allDrawsResults:
-        print(createDateDescription(resultSet['date']))
-        winningNumbers={}
-        #WinningNumbers per draw are contained in a 'content' dict under the key 'winningNumbers' 
-        for drawResult in resultSet['content']:
-            #Itterate over each winningNumber and then add it in winningNumbers dict as a key and increment its occurences counter by one
-            for winningNumber in drawResult['winningNumbers']['list']:
-                winningNumbers[winningNumber] = winningNumbers.get(winningNumber, 0) + 1
+    if(not allDrawsResults):
+        print("No results to show yet")
+    else:
+        #Itterate over every result per day that contains all 180 draw results of the day
+        for resultSet in allDrawsResults:
+            print(createDateDescription(resultSet['date']))
+            winningNumbers={}
+            #WinningNumbers per draw are contained in a 'content' dict under the key 'winningNumbers' 
+            for drawResult in resultSet['content']:
+                #Itterate over each winningNumber and then add it in winningNumbers dict as a key and increment its occurences counter by one
+                for winningNumber in drawResult['winningNumbers']['list']:
+                    winningNumbers[winningNumber] = winningNumbers.get(winningNumber, 0) + 1
 
-        print("All winning numbers occurences")
-        print(winningNumbers)
-        maxOccurences = max(winningNumbers.values())
-        numbersWithMaxOccurences = [key for key in winningNumbers.keys() if winningNumbers[key] == maxOccurences]
-        print("Max occurence is {max}. {textNumber} with such occurence {textIs}: ".format(max=maxOccurences, textNumber= "Numbers" if len(numbersWithMaxOccurences)>1 else "Number", textIs=  "are" if len(numbersWithMaxOccurences)>1 else "is"))
-        print(numbersWithMaxOccurences)
+            print("All winning numbers occurences")
+            print(winningNumbers)
+            maxOccurences = max(winningNumbers.values())
+            numbersWithMaxOccurences = [key for key in winningNumbers.keys() if winningNumbers[key] == maxOccurences]
+            print("Max occurence is {max}. {textNumber} with such occurence {textIs}: ".format(max=maxOccurences, textNumber= "Numbers" if len(numbersWithMaxOccurences)>1 else "Number", textIs=  "are" if len(numbersWithMaxOccurences)>1 else "is"))
+            print(numbersWithMaxOccurences)
 
 
 #----------------Utils-------------------------
